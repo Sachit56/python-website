3feed4a4e9ad584660cd17efbb540567fb419c97
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-17 20:57:28 +0545

    Simulation Completed

--- api/receiver.py
+++ api/receiver.py
@@ -1,6 +1,7 @@
 import socket
 from threading import Thread
 from taipy.gui import Gui, State, invoke_callback, get_state_id
+import pandas as pd
 
 HOST = "127.0.0.1"
 PORT = 5050
@@ -30,10 +31,49 @@ def client_handler(gui: Gui, state_id_list: list):
 
 def update_received_data(state: State, val):
     state.received_data = val
+    temp_data=state.data
+    temp_data["Price"][3]=state.received_data.split(",")[0]
+    state.plot_location=state.received_data.split(",")[1]
+    state.data=temp_data
 received_data = 0
+data = {
+    "Date": pd.date_range("2023-04-12", periods=4, freq="D"),
+    "Price": [2200000, 19700000, 270000, 6.5],
+    
+}
+
+title = "Real State Simulator using python"
+logo = "logo.jpeg"
+plot_location = 'Bhaktapur'
+plot_max_price = 500000000
+plot_min_price = 100000000
+
+def run(state):
+    print(f'{plot_max_price} is maximum.')
+
+    print(state.plot_location)
+
+    with open("data.txt","a") as f:
+        f.write(f'{state.plot_location} ,{state.plot_max_price} ,{state.plot_min_price}\n')
 
 md = """
-Received Data: <|{received_data}|>
+<| text-center
+<|{logo}|image|>
+
+<|{title}|>
+
+Location of Plot:<|{plot_location}|input|>
+
+Maximum Price:<|{plot_max_price}|number|>
+
+Minimum Price:<|{plot_min_price}|number|>
+
+<|Submit|button|on_action=run|>
+
+<|{data}|chart|mode=lines|x=Date|y[1]=Price|line[1]=red|>
+
+|>
+Received Data:<|{received_data}|>
 """
 gui = Gui(page=md)
 
--- api/sender.py
+++ api/sender.py
@@ -1,3 +1,4 @@
+
 import time
 import socket
 
@@ -8,19 +9,25 @@ PORT = 5050
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    
+
     while True:
-        with open("../data.txt","r") as f:
-            data=f.read()
+        with open("../data.txt", "r") as f:
+            data = f.read()
         values = [val.strip() for val in data.split(",")]
 
-        # Check if the split produced at least three values
-        if len(values) >= 3:
-            name, max_val, min_val = values[:3]  # Take the first three values
+        
+        if len(values) == 3:
+            name, max_val, min_val = values 
             print(name, min_val, max_val)
 
-     
-        random_number1 = randint(int(min_val), int(max_val))
-        s.sendall(f"{random_number1},{name}".encode())
-        print(f"Sending: {random_number1}{name}")
+            
+            try:
+                random_number1 = randint(int(min_val), int(max_val))
+                print(f"Generated random number: {random_number1} {name}")
+                s.sendall(f"{random_number1},{name}".encode())
+            except ValueError as e:
+                print(f"Error converting to integers: {e}")
+        else:
+            print("Error: Unexpected format in data.txt")
+
         time.sleep(2)
