9135a8cc336c4f24456f41b9f362988c2b4e7276
Sachit Khadka <sachitkhadka73@gmail.com>
2023-12-17 14:49:38 +0545

    Random Real state prediction

--- api/index.py
+++ api/index.py
@@ -1,10 +1,73 @@
-from http.server import BaseHTTPRequestHandler
+# from taipy import Gui
+# import pandas as pd
 
-class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+# data = {
+#           "Date": pd.date_range("2023-04-12", periods=100, freq="D"),
+#           "Temp°C": [-15,-12.9,7.2,10.2],
+#           "Min": [-22,-19.7,2.7,6.5],
+#           "Max": [-8.6,-8.2,12.,13.5]
+
+#   }
+
+# title="Real State Simulator using python"
+# logo="logo.jpeg"
+# plot_location='Bhaktapur'
+# plot_max_price=5000000
+# plot_min_price=1000000
+# page="""
+
+# <| text-center
+# <|{logo}|image|>
+
+# <|{title}|>
+
+# Location of Plot:<|{plot_location}|input|>
+
+# Maximum Price:<|{plot_max_price}|number|>
+
+# Minimum Plot:<|{plot_min_price}|number|>
+
+# <|{data}|chart|mode=lines|x=Date|y[1]=Temp°C|y[2]=Min|y[3]=Max|line[1]=dash|color[2]=blue|color[3]=red|>
+
+# |>
+# """
+
+# if __name__=="__main__":
+#     app=Gui(page)
+#     app.run(use_reloader=True)
+
+from taipy import Gui
+import pandas as pd
+
+data = {
+    "Date": pd.date_range("2023-04-12", periods=4, freq="D"),
+    "Min": [-22, -19.7, 2.7, 6.5],
+    "Max": [-8.6, -8.2, 12., 13.5]
+}
+
+title = "Real State Simulator using python"
+logo = "logo.jpeg"
+plot_location = 'Bhaktapur'
+plot_max_price = 5000000
+plot_min_price = 1000000
+page = """
+<| text-center
+<|{logo}|image|>
+
+<|{title}|>
+
+Location of Plot:<|{plot_location}|input|>
+
+Maximum Price:<|{plot_max_price}|number|>
+
+Minimum Plot:<|{plot_min_price}|number|>
+
+<|{data}|chart|mode=lines|x=Date|y[1]=Min|y[2]=Max|line[1]=dash|color[2]=blue|>
+
+|>
+"""
+
+if __name__ == "__main__":
+    app = Gui(page)
+    app.run(use_reloader=True)
